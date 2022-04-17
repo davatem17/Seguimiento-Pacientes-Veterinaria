4b3bb43f7f9443385f3d10fbd10a6fd1f3947b42
davatem17 <perezd625@gmail.com>
2022-04-16 21:44:12 -0500

    Primero

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+function App() {
+  return (
+    <div className="container mx-auto mt-20">
+      <Header/>
+      <Formulario/>
+    </div>
+  )
+}
+
+export default App
